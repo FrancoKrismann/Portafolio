e82dd6adef98c6af9d34a51f6d9a3a9347504603
Kris0707 <francokris005@gmail.com>
2023-08-03 21:10:38 -0300

    Contacto-Arreglado

--- src/components/Sections/Home/Home.jsx
+++ src/components/Sections/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {HomeContainer} from "../../../style"
 import styles from "./styles.module.css"
-import image from "../../../images/profile-pic (5).png"
 
 export default function Home() {
   return (
@@ -11,9 +10,7 @@ export default function Home() {
             <h2>Mi nombre es Franco Krismann</h2>
             <h3>Desarrollador Web Full Stack</h3>
         </div>
-        <div className={styles.containerImage}>
-            <img src={image} alt="perfil"/>
-        </div>
+        
     </HomeContainer>
   )
 }
